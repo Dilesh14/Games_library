e5c4f8de9bece595c75cc228d22d8215ce8b6049
Dilesh <drj855@mun.ca>
2019-02-06 23:03:35 -0330

    upto databases

---
+++ catalog/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from catalog.models import Game, Publisher
+# Register your models here.
+admin.site.register(Game)
+admin.site.register(Publisher)
---
+++ catalog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    publisher= models.ForeignKey('Publisher',on_delete=models.SET_NULL,null=True)
+    summary= models.TextField(max_length=1000,help_text='Enter a brief description of the book')
+    
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('game-detail',args=[str(self.id)])
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+    address= models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('publisher-detail',args=[str(self.id)])
+    
