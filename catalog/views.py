8ffc29c07a43c4976fb5036d02a90adcd0bcdd89
Dilesh <drj855@mun.ca>
2019-02-07 18:07:16 -0330

    Upto homepage

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from catalog.models import Game, Publisher
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_games= Game.objects.all().count()
+    
+    num_publisher = Publisher.objects.all().count()
+        
+    
+    # The 'all()' is implied by default.    
+   
+    
+    context = {
+        'num_games': num_games,
+        'num_publisher': num_publisher,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
